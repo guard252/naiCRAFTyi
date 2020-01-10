3c129d6fc24b3a0888e2bb6ea30643acd1c25ff3
guard252 <danylotv43@gmail.com>
2020-01-10 16:08:51 +0200

    Upgraded VAO class to be able to change buffer after it was created

--- src/GLAbstraction/VBO.cpp
+++ src/GLAbstraction/VBO.cpp
@@ -1,25 +1,44 @@
 #include "VBO.h"
 
+namespace GL
+{
+    VBO::VBO(GLuint size, GLfloat *data)
+    {
+        GLCall(glGenBuffers(1, &rendererID));
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, rendererID));
+        GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_DYNAMIC_DRAW));
+    }
 
-GL::VBO::VBO(GLuint size, GLfloat *data) {
-    GLCall(glGenBuffers(1, &rendererID));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, rendererID));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_DYNAMIC_DRAW));
-}
+    void VBO::Bind() const
+    {
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, rendererID));
 
-void GL::VBO::Bind() const {
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, rendererID));
+    }
 
-}
+    void VBO::UnBind() const
+    {
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
 
-void GL::VBO::UnBind() const {
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    }
 
-}
+    VBO::~VBO()
+    {
+        GLCall(glDeleteBuffers(1, &rendererID));
+    }
 
-GL::VBO::~VBO() {
-    GLCall(glDeleteBuffers(1, &rendererID));
+    //The same as constructor
+    void VBO::GenerateBuffer(GLuint size, GLfloat *data)
+    {
+        GLCall(glGenBuffers(1, &rendererID));
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, rendererID));
+        GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_DYNAMIC_DRAW));
+    }
 
+    void VBO::UpdateBuffer(GLuint size, GLfloat *data)
+    {
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, rendererID));
+        GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_DYNAMIC_DRAW));
+    }
 }
 
 
--- src/GLAbstraction/VBO.h
+++ src/GLAbstraction/VBO.h
@@ -6,8 +6,10 @@ namespace GL {
     private:
         GLuint rendererID;
     public:
+        VBO(){}
         VBO(GLuint size, GLfloat *data);
-
+        void GenerateBuffer(GLuint size, GLfloat *data);
+        void UpdateBuffer(GLuint size, GLfloat *data);
         void Bind() const;
 
         void UnBind() const;
